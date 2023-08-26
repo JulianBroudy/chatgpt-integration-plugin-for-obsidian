f7b11609bf7c08bbd86442b35533ca0dc7b1b4a9
Julian Broudy <Julian@Broudy.io>
2023-08-26 23:14:44 +0300

    Improve commit view.

--- src/api/CommitView.ts
+++ src/api/CommitView.ts
@@ -1,25 +1,18 @@
-import {ItemView, WorkspaceLeaf} from "obsidian";
+import {ItemView, TFile, TFolder, WorkspaceLeaf} from "obsidian";
 import {StagedChange, StagingArea} from "./StaginArea";
-import LOGGER from "../utils/Logger";
 
 export const COMMIT_VIEW_TYPE = "commit-view";
 
 export class CommitView extends ItemView {
 	private stagingArea: StagingArea;
-	private changeElements: Map<StagedChange, HTMLElement>;
-	private documentIdToChange: Map<string, StagedChange>;
-	private stagedContainer;
-	private unstagedContainer;
+	private rootContainer: HTMLElement;
+	private readonly indentation = 21;
 
 	constructor(leaf: WorkspaceLeaf, stagingArea: StagingArea) {
 		super(leaf);
 		this.stagingArea = stagingArea;
-		this.changeElements = new Map();
-		this.documentIdToChange = new Map();
-		this.stagedContainer = this.contentEl.createDiv('staged-changes resizable');
-		this.stagedContainer.createEl('h3', {text: 'Staged Changes'});
-		this.unstagedContainer = this.contentEl.createDiv('unstaged-changes resizable');
-		this.unstagedContainer.createEl('h3', {text: 'Unstaged Changes'});
+		this.rootContainer = this.contentEl.createDiv('changes-root');
+		this.rootContainer.createEl('h3', {text: 'Changes'});
 		this.load();
 	}
 
@@ -36,88 +29,102 @@ export class CommitView extends ItemView {
 	}
 
 	private async initializeView() {
-		// Clear the existing content
-		// this.contentEl.empty();
 		await this.stagingArea.computeChangedFiles();
+		const allChanges = [...this.stagingArea.getStagedChanges(), ...this.stagingArea.getUnstagedChanges()];
+		const changeMap = new Map(allChanges
+			.filter(change => change.document.sourceId !== undefined)
+			.map(change => [change.document.sourceId!, change]));
 
-		// Create a container for the staged changes
-
-
-		this.stagingArea.getStagedChanges().forEach(change => {
-			this.createChangeItem(this.stagedContainer, change, true);
-		});
-
-		// Create a container for the unstaged changes
-		const unstagedChanges = this.stagingArea.getUnstagedChanges();
-		LOGGER.info("Unstaged changes: ", unstagedChanges)
-		unstagedChanges.forEach(change => {
-			LOGGER.info("Creating unstaged item");
-			this.createChangeItem(this.unstagedContainer, change, false);
-		});
-
-		// Create a commit button
-		const commitButton = this.contentEl.createEl('button', {text: 'Commit', cls: 'mod-cta'});
-		commitButton.addEventListener('click', () => {
-			this.stagingArea.commitChanges();
-			// Refresh the view or perform other actions after committing
-		});
-
-		// To allow dropping
-		['dragover', 'drop'].forEach(eventName => {
-			[this.stagedContainer, this.unstagedContainer].forEach(container => {
-				container.addEventListener(eventName, (event) => {
-					event.preventDefault();
-					event.stopPropagation();
-				});
-
-				container.addEventListener('drop', (event) => {
-					const id = event.dataTransfer!.getData('text/plain');
-
-					const change = this.documentIdToChange.get(id);
-					const item = this.changeElements.get(change!);
-					if (item && item.parentElement !== container) {
-						container.appendChild(item);
-						// Update stage/unstage logic here
-						if (container === this.stagedContainer) {
-							this.stagingArea.stageChange(change!);
-						} else {
-							this.stagingArea.unstageChange(change!);
-						}
-					}
-				});
-			});
-		});
-
+		const root = this.app.vault.getRoot();
+		this.renderFolder(root, this.rootContainer, 0, changeMap);
 	}
 
-	private createChangeItem(container: HTMLElement, change: StagedChange, isStaged: boolean): HTMLElement {
-		const item = container.createDiv('change-item');
-		item.createEl('span', {text: `${change.document.documentId}`});
-		item.addClass(change.type);
-		// Make it draggable
-		item.draggable = true;
+	private renderFolder(folder: TFolder, parentElement: HTMLElement, level: number, changeMap: Map<string, StagedChange>): number {
+		const folderDiv = parentElement.createDiv({cls: 'folder-item'});
+		folderDiv.style.marginLeft = `${level * this.indentation}px`; // Indentation
 
-		item.addEventListener('dragstart', (event) => {
-			event.dataTransfer!.setData('text/plain', change.document.documentId!);
+		const folderLine = folderDiv.createDiv({cls: 'folder-line'});
+		folderLine.addEventListener('dblclick', () => {
+			this.toggleFolder(arrowContainer, childrenDiv, rightArrow, downArrow);
 		});
 
-		// Double click to move
-		item.addEventListener('dblclick', () => {
-			if (item.parentElement === this.stagedContainer) {
-				this.stagingArea.unstageChange(change);
-				this.unstagedContainer.appendChild(item);
-			} else {
-				this.stagingArea.stageChange(change);
-				this.stagedContainer.appendChild(item);
+		const arrowContainer: HTMLElement = folderLine.createEl('div', {cls: 'arrow-container'});
+		const rightArrow: HTMLElement = arrowContainer.createEl('div');
+		rightArrow.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>';  // Lucide "chevron-right"
+		const downArrow: HTMLElement = arrowContainer.createEl('div');
+		downArrow.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>';  // Lucide "chevron-down"
+		downArrow.style.display = 'none'; // Initially hidden
+
+		folderLine.createEl('div', {cls: 'line-section'}).createEl('input', {type: 'checkbox', cls: 'no-margin'});  // Checkbox
+
+		const folderIcon: HTMLElement = folderLine.createEl('div');
+		folderIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="#43454A" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder"><path d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z"/></svg>';  // Lucide "folder-git"
+
+		folderLine.createEl('span', {text: folder.isRoot() ? app.vault.getName() : folder.name, cls:'item-text'});
+
+		const childrenDiv = parentElement.createDiv({cls: 'folder-children'});
+		childrenDiv.style.display = 'none';  // Initially collapsed
+
+		// Toggle folder on click
+		arrowContainer.addEventListener('click', () => {
+			this.toggleFolder(arrowContainer, childrenDiv, rightArrow, downArrow);
+		});
+
+
+		let visibleFileCount = 0;  // Initialize count of visible files in this folder and its subfolders
+
+		// Loop through child folders first
+		for (const child of folder.children.filter(c => c instanceof TFolder)) {
+			visibleFileCount += this.renderFolder(child as TFolder, childrenDiv, level + 1, changeMap);
+		}
+
+		// Loop through child files next
+		for (const child of folder.children.filter(c => c instanceof TFile)) {
+			if (changeMap.has((child as TFile).path)) {
+				this.renderFile(child as TFile, childrenDiv, level + 2, changeMap.get((child as TFile).path));
+				visibleFileCount++;  // Increase the count of visible files
 			}
-		});
+		}
 
-		// Update the map
-		this.changeElements.set(change, item);
-		this.documentIdToChange.set(change.document.documentId!, change);
+		// Hide folder if it has no visible children
+		if (visibleFileCount === 0) {
+			folderDiv.style.display = 'none';
+		} else {
+			// Add "x files" next to the folder name
+			folderLine.createEl('span', {text: `${visibleFileCount} files`, cls: 'folder-file-count'});
+		}
 
-		return item;
+		return visibleFileCount;  // Return the count of visible files
 	}
 
+	private renderFile(file: TFile, parentElement: HTMLElement, level: number, change: StagedChange | undefined) {
+		const fileDiv = parentElement.createDiv({cls: 'file-item'});
+		fileDiv.style.marginLeft = `${level * this.indentation}px`; // Indentation
+		fileDiv.createEl('div', {cls: 'line-section'}).createEl('input', {type: 'checkbox', cls: 'no-margin'});
+		fileDiv.createEl('span', {text: file.name, cls: `item-text ${change?.type}`});
 
+		if (change && this.stagingArea.getStagedChanges().includes(change)) {
+			fileDiv.querySelector('input')!.checked = true;
+		}
+	}
+
+	private toggleFolder(arrowContainer: HTMLElement, childrenDiv: HTMLElement, rightArrow: HTMLElement, downArrow: HTMLElement, collapseOnly: boolean = false) {
+		if (childrenDiv.style.display === 'none' && !collapseOnly) {
+			childrenDiv.style.display = 'block';
+			rightArrow.style.display = 'none';
+			downArrow.style.display = 'block';
+		} else {
+			childrenDiv.style.display = 'none';
+			rightArrow.style.display = 'block';
+			downArrow.style.display = 'none';
+
+			// Collapse all sub-folders
+			childrenDiv.querySelectorAll('.folder-children').forEach((childDiv: HTMLElement) => {
+				const childArrowContainer = childDiv.previousElementSibling!.querySelector('.arrow-container') as HTMLElement;
+				const childRightArrow = childArrowContainer.querySelector('.lucide-chevron-right')!.parentElement as HTMLElement;
+				const childDownArrow = childArrowContainer.querySelector('.lucide-chevron-down')!.parentElement as HTMLElement;
+				this.toggleFolder(childArrowContainer, childDiv as HTMLElement, childRightArrow, childDownArrow, true);
+			});
+		}
+	}
 }
