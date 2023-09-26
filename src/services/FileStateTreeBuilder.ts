f92d2fd552f60ce853d048b97de9927ba6e85a20
Julian Broudy <Julian@Broudy.io>
2023-09-26 12:10:51 +0700

    Introduce a builder for proper file-system structure simulation.

---
+++ src/services/FileStateTreeBuilder.ts
@@ -0,0 +1,70 @@
+import {DataStore} from "./datastore";
+import FileStateTree from "../models/FileStateTree";
+import {DocumentMetadata, Source} from "../models/models";
+import {TFile} from "obsidian";
+import {FileState} from "../models/TreeNode";
+import LOGGER from "../utils/Logger";
+
+export default class FileStateTreeBuilder {
+	private datastore: DataStore;
+
+	constructor(datastore: DataStore) {
+		this.datastore = datastore;
+	}
+
+	async build() {
+		const fileStateTree = new FileStateTree(app.vault.getRoot());
+		const obsidianFilesMap = new Map<string, TFile>(app.vault.getAllLoadedFiles()
+			.filter(f => f instanceof TFile)
+			.map(f => [(f as TFile).basename, f as TFile]));
+		console.log(obsidianFilesMap)
+		app.vault.getAllLoadedFiles().filter(f => f instanceof TFile).forEach(f => LOGGER.info((f as TFile).basename));
+		const syncedFiles = await this.fetchSyncedFiles();
+		console.log("Synced Files" + syncedFiles)
+		this.populateFileStateTree(fileStateTree, obsidianFilesMap, syncedFiles);
+		return fileStateTree;
+	}
+
+	private populateFileStateTree(fileStateTree: FileStateTree, obsidianFilesMap: Map<string, TFile>, syncedFiles: DocumentMetadata[]) {
+		syncedFiles.forEach(syncedFile => {
+			const createdAtDate = new Date(<string>syncedFile.createdAt);
+			const loadedFile = obsidianFilesMap.get(syncedFile.sourceId);
+
+			if (loadedFile) {
+				const mtimeDate = new Date(loadedFile.stat.mtime);
+				const ctimeDate = new Date(loadedFile.stat.ctime);
+				LOGGER.silly('DB File:' + syncedFile.sourceId + '\ncreatedAtDate:' + createdAtDate);
+				LOGGER.silly('FS File:' + loadedFile.basename + '\nmtimeDate:' + mtimeDate + '\nctimeDate:' + ctimeDate);
+				if (createdAtDate < mtimeDate) {
+					LOGGER.silly('createdAtDate < mtimeDate:' + createdAtDate + ' < ' + mtimeDate);
+					this.addNode(fileStateTree,loadedFile, FileState.MODIFIED, syncedFile);
+				}
+				obsidianFilesMap.delete(syncedFile.sourceId); // Remove from map to identify new files later
+			} else if (syncedFile.source == Source.FILE) {
+				LOGGER.silly("DELETED: " + loadedFile)
+				LOGGER.silly("DELETED: " + syncedFile.sourceId)
+				// TODO: find a way to handle deleted files
+				this.addNode(fileStateTree,new TFile(), FileState.DELETED, syncedFile);
+				// this.addChange({document: syncedFile, type: 'deleted'}); // File is in database but not in Obsidian
+			}
+		});
+
+		// Process remaining Obsidian files that are not in the database (new files)
+		obsidianFilesMap.forEach(loadedFile => {
+			this.addNode(fileStateTree,loadedFile, FileState.NEW, new DocumentMetadata());
+		});
+	}
+
+
+	private async fetchSyncedFiles() {
+		const newlyFetchedFiles = await this.datastore.getSyncedDocuments();
+		LOGGER.info('Fetched synced files: \n' + newlyFetchedFiles);
+		return newlyFetchedFiles;
+	}
+
+	private addNode(fileStateTree: FileStateTree,loadedFile: TFile, MODIFIED: FileState, syncedFile: DocumentMetadata) {
+		fileStateTree.logMapState();
+		fileStateTree.addNode(loadedFile,MODIFIED,syncedFile);
+		fileStateTree.logMapState();
+	}
+}
